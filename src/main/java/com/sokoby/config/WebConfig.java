e701534e9dca643228e53e36f652d137974b3eb0
aaravsoni1 <soniaarav596@gmail.com>
2025-04-07 00:59:35 +0530

    added web config

---
+++ src/main/java/com/sokoby/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.sokoby.config;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import jakarta.servlet.MultipartConfigElement;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    @Primary
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(10)); // 10MB max file size
+        factory.setMaxRequestSize(DataSize.ofMegabytes(50)); // 50MB max request size
+        return factory.createMultipartConfig();
+    }
+} 
